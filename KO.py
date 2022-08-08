b81b57582fdb31c3dad8a2126e1da1e5fcec3a7d
darrelllarsen <110845926+darrelllarsen@users.noreply.github.com>
2022-08-08 13:37:02 -0400

    Initial commit

---
+++ KO.py
@@ -0,0 +1,234 @@
+﻿#KO: Basic Tools for Working with Korean Text
+#Version 0.5.1
+#Author: Darrell Larsen
+#
+#Distributed under GNU General Public License v3.0
+
+
+"""Section: Conversion between Korean syllables (as Unicode object strings) and their component sounds.
+
+For simple conversion of a Korean syllable to its ordinal value, or conversion from an ordinal value to a Korean syllable, use Python's built-in ord() and chr() functions.
+"""
+# Key:(ord_value, Yale_romanization)
+initials = {'ㄱ':(0,'k'),'ㄲ':(1,'kk'),'ㄴ':(2,'n'),'ㄷ':(3,'t'),'ㄸ':(4,'tt'),
+'ㄹ':(5, 'l'),'ㅁ':(6,'m'),'ㅂ':(7,'p'),'ㅃ':(8,'pp'),'ㅅ':(9,'s'),'ㅆ':(10,'ss'),
+'ㅇ':(11,''),'ㅈ':(12,'c'),'ㅉ':(13,'cc'),'ㅊ':(14,'ch'),'ㅋ':(15,'kh'),'ㅌ':(16,'th'),
+'ㅍ':(17,'ph'),'ㅎ':(18,'h')}
+medials = {'ㅏ':(0,'a'),'ㅐ':(1,'ay'),'ㅑ':(2,'ya'),'ㅒ':(3,'yay'),'ㅓ':(4,'e'),
+'ㅔ':(5,'ey'),'ㅕ':(6,'ye'),'ㅖ':(7,'yey'),'ㅗ':(8,'o'),'ㅘ':(9,'wa'),'ㅙ':(10,'way'),
+'ㅚ':(11,'oy'), 'ㅛ':(12,'yo'),'ㅜ':(13,'wu'),'ㅝ':(14,'we'),'ㅞ':(15,'wey'),'ㅟ':(16,'wi'),
+'ㅠ':(17,'yu'),'ㅡ':(18,'u'),'ㅢ':(19,'uy'),'ㅣ':(20,'i')}
+finals = {'':(0,''),'ㄱ':(1,'k'),'ㄲ':(2,'kk'),'ㄳ':(3,'ks'),'ㄴ':(4,'n'),
+'ㄵ':(5,'nc'),'ㄶ':(6,'nh'),'ㄷ':(7,'t'),'ㄹ':(8,'l'),'ㄺ':(9,'lk'),'ㄻ':(10,'lm'),
+'ㄼ':(11,'lp'),'ㄽ':(12,'ls'),'ㄾ':(13,'lth'),'ㄻ':(14,'lm'),'ㅀ':(15,'lh'),'ㅁ':(16,'m'),
+'ㅂ':(17,'p'),'ㅄ':(18,'ps'),'ㅅ':(19,'s'),'ㅆ':(20,'ss'),'ㅇ':(21,'ng'),'ㅈ':(22,'c'),
+'ㅊ':(23,'ch'),'ㅋ':(24,'kh'),'ㅌ':(25,'th'),'ㅍ':(26,'ph'),'ㅎ':(27,'h')}
+
+"""Yale Patterns - Unnecessary
+Yale_initial_letters = {'ㄱ':'k','ㄲ':'kk','ㄴ':'n','ㄷ':'t','ㄸ':'tt','ㄹ':'l','ㅁ':'m',
+'ㅂ':'p','ㅃ':'pp','ㅅ':'s','ㅆ':'ss','ㅇ':'','ㅈ':'c','ㅉ':'cc','ㅊ':'ch','ㅋ':'kh',
+'ㅌ':'th','ㅍ':'ph','ㅎ':'h'}
+Yale_medial_letters = {'ㅏ':'a','ㅐ':'ay','ㅑ':'ya','ㅒ':'yay','ㅓ':'e','ㅔ':'ey','ㅕ':'ye',
+'ㅖ':'yey','ㅗ':'o','ㅘ':'wa','ㅙ':'way','ㅚ':'oy','ㅛ':'yo','ㅜ':'wu','ㅝ':'we','ㅞ':'wey',
+'ㅟ':'wi','ㅠ':'yu','ㅡ':'u','ㅢ':'uy','ㅣ':'i'}
+Yale_final_letters = {'':'','ㄱ':'k','ㄲ':'kk','ㄳ':'ks','ㄴ':'n','ㄵ':'nc','ㄶ':'nh',
+'ㄷ':'t','ㄹ':'l','ㄺ':'lk','ㄻ':'lm','ㄼ':'lp','ㄽ':'ls','ㄾ':'lth','ㄻ':'lm','ㅀ':'lh',
+'ㅁ':'m','ㅂ':'p','ㅄ':'ps','ㅅ':'s','ㅆ':'ss','ㅇ':'ng','ㅈ':'c','ㅊ':'ch','ㅋ':'kh',
+'ㅌ':'th','ㅍ':'ph','ㅎ':'h'}
+
+"""
+
+def _get_split_values(a):
+    """get_split_values(str) -> tuple (int, int, int).
+
+    Split a one-character syllable input up into letters. Returns a tuple containing the 
+    conversion values for the letters (initial letter, medial letter, final letter)"""
+    
+    value_a = ord(a)
+    fin = ((value_a-44032) % 588) % 28
+    mid = (((value_a - 44032) % 588) - fin) / 28
+    init = ((value_a -44032) - mid*28 - fin) / 588
+    return (int(init), int(mid), int(fin))
+
+#Given three Korean letters (onset, nucleus, coda), returns a tuple containing their conversion values.
+def _get_letters(value_set):
+    #Argument must be ordered set of 3 or 2 (if no final letter is present)
+    for key in initials:
+        if initials[key][0] == value_set[0]:
+            init = key
+    for key in medials:
+        if medials[key][0] == value_set[1]:
+            mid = key
+    for key in finals:
+        if finals[key][0] == value_set[2]:
+            fin = key
+    return (init, mid, fin)
+
+#Find the ordinal value of a Korean syllable containing the three letters passed in as arguments.
+def _get_combined_value(a,b,c):
+    return initials[a][0]*588 + medials[b][0]*28 + finals[c][0] + 44032
+
+def _values_to_letter(a):
+    return chr(_get_combined_value(a[0],a[1],a[2]))
+   
+def _combine_value_list(a):
+    return _values_to_letter(_get_letters(a))
+
+#Given three ordered conversion values, return a Korean syllable.
+def combine(a,b,c):
+    return chr(_get_combined_value(a,b,c))
+
+def split(a):
+    return _get_letters(_get_split_values(a))
+
+
+
+"""Section: Tests for Korean input."""
+
+def isSyllable(text):
+    #returns true if input text is a single Korean syllable; input must be a string
+    #1-character strings evaluated as KO syllables or not; 2-character strings all return False
+    #currently only returns true for modern Korean syllables
+    if len(text) == 1 and 44032 <= ord(text) <= 55203:
+        return True
+    else:
+        return False
+
+
+def isMultigraph(letter):
+    #Check for clusters found in coda positions
+    
+    #check in U3130 set (12593-12686)
+    if ord(letter) in {12595,12597,12598,12602,12603,12604,12605,12606,12607,12608,12612}:
+        return True
+    else:
+        return False
+	
+    #check in U1100 set (4342-4469)
+    #check in UA960 set (43360-43388)
+    #check in UD7B0 set (55216-55291)
+
+
+
+def isLetter(letter):
+    value = ord(letter)
+    #check in U3130 set (12593-12686)
+    if value >= 12593 and value <= 12686:
+        return True
+	
+    #check in U1100 set (4342-4469)
+    elif value >= 4352 and value <= 4607:
+        return True
+
+    #check in UA960 set (43360-43388)
+    elif value >= 43360 and value <= 43388:
+        return True
+
+    #check in UD7B0 set (55216-55291), and exclude empty values
+    elif value >= 55216 and value <= 55291 and not (value >= 55239 and value <= 55242):
+        return True
+
+    else:
+        return False
+
+def isHangul(text):
+    if isSyllable(text) or isLetter(text):
+        return True
+    else:
+        return False
+
+
+
+
+
+""""Romanization"""
+def toYale(text, syllable=None, WO=False, U=False, strict=False):
+    """Options: 
+    syllable = max (will place a dot between all syllables in a word)
+    syllable = min (limits dots to official Yale rules, with a few extra)
+    syllable = none (no syllables boundaries marked)
+    WO = True (will use <wo> instead of <o> for ㅗ.  Useful for some time periods)
+    U = True (will use <u> after labial consonants)
+    strict = True (follows Yale Romanization rules as closely as possible, without
+    phonological knowledge; currently, this sets WO and U to true, and syllable to min, 
+    regardless what the user entered"""
+    
+    output = ''
+    prev_letters = None
+    if strict == True:
+        WO = True
+        U = True
+        syllable = 'min'
+   
+    for char in text:
+        if isHangul(char) and isSyllable(char):
+            letters = split(char)
+            onset = initials[letters[0]][1]
+            nucleus = medials[letters[1]][1]
+            coda = finals[letters[2]][1]
+
+            if WO == True and nucleus == 'o':
+                nucleus = 'wo'
+            if U == True and nucleus == 'wu' and onset in {'p','pp','ph','m'}:
+                nucleus = 'u' 
+
+            #syllable = 'max': places dot between all syllables
+            if prev_letters != None and syllable == 'max':
+                output = output + '.'
+
+            #syllable = 'min': places dot between ambiguous syllables only    
+            elif prev_letters != None and syllable == 'min':
+                prev_coda = prev_letters[2]
+                prev_vowel = prev_letters[1]
+            
+                if prev_coda == '':
+                    #Mark after coda-less syllables when C follows
+                    if onset != '':
+                        output = output + '.'
+
+                    #Mark between adjacent vowels, when confusion of <w>/<y> 
+                    #and <a>, <e>, <o>, <u> may arise
+                    elif ((prev_vowel[len(prev_vowel)-1] in {'a','e','o','u'} and \
+                            nucleus == 'y') \
+                            or (prev_vowel[len(prev_vowel)-1] in {'y', 'w'} and \
+                            nucleus in {'a','e','o','u'})):
+                        output = output + '.'
+
+                else:        
+                    #Mark before onsetless syllables, unless previous coda was 
+                    #null or <ng>
+                    if prev_coda not in {'','ng'} and onset == '':
+                        output = output + '.'
+                        
+
+                    #Mark after a consonant (final one in case of cluster) when onset 
+                    #consonant is same, for those which can be doubled
+                    elif prev_coda[len(prev_coda)-1] == onset and onset in \
+                        {'k','p','t','c','s'} and prev_coda not in \
+                        {'kk','pp','tt','cc','ss'}:
+                        output = output + '.'
+
+                    #Mark after a consonant when onset of following is <h>, when 
+                    #ambiguity can arise
+                    elif prev_coda[len(prev_coda)-1] in {'k','p','t','c'} and \
+                            onset in {'h'}:
+                        output = output + '.'
+
+                    #Mark when <k> is followed by <kh>.  Need not do for <p,t,c>, i
+                    #as their doubled variants do not occur in coda position    
+                    elif prev_coda in {'k'} and onset in {'kh'}:
+                        output = output + '.'
+
+            output = (output + onset + nucleus + coda) 
+            prev_letters = (onset,nucleus,coda)
+ 
+        else:
+            output = output + char
+            prev_letters = None
+    return output            
+ 
+
+
+
+
+
---
+++ kre.py
@@ -0,0 +1,331 @@
+﻿#KRE: Support for Korean regular expression searches.
+#Version 0.5.1
+#Author: Darrell Larsen
+#
+#Distributed under GNU General Public License v3.0
+
+"""IMPLEMENTED: COMPILE, FINDALL, FINDITER, FULLMATCH, MATCH, SEARCH
+MOSTLY IMPLEMENTED: KRE.Match OBJECTS
+
+This module is built on top of re.  See re documentation for information
+on special characters, etc, not affected by this module.
+
+This module provides support for conducting regular expression searches
+over Unicode strings containing Korean text.  This is designed for users
+wishing to search for Korean text at the sub-syllable level; those wishing 
+to conducted re searches using full Korean syllables can do so using the 
+standard re module.  This module breaks Korean syllables up 
+into their composite letters in both the regular expression and the text 
+to be searched prior to conducting the search.  By default, indices are 
+then calculated based on the original input text, and a kre.SRE_Match 
+object is returned.  kre.KRE_Match objects are designed to behave like
+_sre.SRE_Match objects, with a few additional attributes.  As such, the 
+output of this module may usually be handled in the same manner as the 
+output of the standard re module. 
+
+With the exception of the set of vowels being placed after the set of 
+consonants, the precedence of characters is based the order used in 
+South Korean dictionaries (see Unicode chart U3130). Older characters 
+are placed after all modern consonants and vowels. 
+
+[ㄱ-ㅎ] : matches all Korean consonants, also includes null ㅇ in initial position
+[ㅏ-ㅣ] : matches all Korean vowels
+[ㄱ-ㅣ] : matches all Korean letters
+
+By default, syllable-final jamo are NOT split (i.e. ㅄ is treated as a 
+single character; it is not split into ㅂㅅ), though the option to split 
+these up may be implemented later.  Likewise, vowels such as ㅖ and ㅢ are
+treated as single characters by default.
+
+By default, kre does NOT track boundaries between syllable characters, even if 
+the search string is entered as a syllabary (e.g. 일 rather than ㅇㅣㄹ).  Thus, 
+a search for 'ㅣㄹ' will match both '일' and '이라'.  To track boundaries, set 
+boundaries=True.  The default boundary symbol is the semicolon <;> and may be 
+changed using boundary_symbol=new_symbol."""
+
+import re, KO
+
+"""Section: Constants"""
+trad_dipthongs = '[ㅑㅒㅖㅛㅠㅘㅙㅝㅞㅟ]'
+dipthongs = '[ㅑㅒㅖㅛㅠㅘㅙㅚㅝㅞㅟㅢ]'
+
+"""Section: kre_MatchObject
+This mimics the _sre.SRE_Match object whenever possible.  A few additional 
+methods are defined to allow the user to obtain data on both the original
+and modified strings created by kre."""
+
+class KRE_Match:
+    def __init__(self, endpos = None, lastgroup = None, lastindex = None, pos = 0, re = None, regs = None, string = None, kre = None, kre_pos = 0, kre_endpos = None, index_mapping = None, kre_string = None):
+       
+        self.endpos =  endpos #will be int; last index position==len(self.string)
+        self.lastgroup = lastgroup
+        self.lastindex = lastindex
+        self.pos = pos #int
+        self.re = re #SRE_Pattern
+        self.regs = regs #tuple
+        self.string = string
+   
+        #Supplemental in KRE; SHOULD WE ALSO DOUBLE ENDPOS, ETC?
+        self.kre = kre #modified KRE_Pattern
+        self.kre_endpos = kre_endpos
+        self.kre_pos = kre_pos
+        self.kre_string = kre_string
+        self.index_mapping = index_mapping
+
+    def __repr__(self):
+        return "<kre.KRE_Match object; span=%s, match='%s'>" % (
+                self.span(), self.string[self.span()[0]:self.span()[1]]) 
+
+    def end(self):
+        """end([group=0]) -> int.
+        Return index of the end of the substring matched by group."""#VERIFIED?
+        return self.regs[0][1]
+
+    def expand():
+        """expand(template) -> str.
+        Return the string obtained by doing backslash substitution
+        on the string template, as done by the sub() method."""#VERIFIED?
+        pass
+    
+    def group(self):
+        """group([group1, ...]) -> str or tuple.
+        Return subgroup(s) of the match by indices or names.
+        For 0 returns the entire match."""#VERIFIED?
+        pass
+
+    def groupdict(self):
+        """groupdict([default=None]) -> dict.
+        Return a dictionary containing all the named subgroups of the match,
+        keyed by the subgroup name. The default argument is used for groups
+        that did not participate in the match"""
+        pass
+
+    def groups(self):
+        """groups([default=None]) -> tuple.
+        Return a tuple containing all the subgroups of the match, from 1.
+        The default argument is used for groups
+        that did not participate in the match"""
+        pass
+
+    def span(self):
+        """span([group]) -> tuple.
+        For MatchObject m, return the 2-tuple (m.start(group), m.end(group))."""
+        return self.regs[0]       
+
+    def start(self):
+        """start([group=0]) -> int.
+        Return index of the start of the substring matched by group."""
+        return self.regs[0][0]
+
+# --------------------------------------------------------------------
+# public interface
+
+def match(pattern, string, flags=0, boundaries=False, boundary_marker=';'):
+    """Try to apply the pattern at the start of the linearized string, returning
+    a kre_match object, or None if no match was found.
+
+    kre modification: source string and pattern are linearized prior to 
+    calling re.match"""
+    match = re.match(_linearize(pattern)[0], _linearize(string, boundaries, boundary_marker)[0], flags)
+
+    if match:
+        return _make_match_object(pattern, string, match, boundaries, boundary_marker) 
+    else:
+        return None
+
+def fullmatch(pattern, string, flags=0, boundaries=False, boundary_marker=';'):
+    """Try to apply the pattern to all of the string, returning
+    a match object, or None if no match was found.
+    
+    kre modification: source string and pattern are linearized prior to 
+    calling re.fullmatch"""
+    match = re.fullmatch(_linearize(pattern)[0], _linearize(string, boundaries, boundary_marker)[0], flags)
+
+    if match:
+        return _make_match_object(pattern, string, match, boundaries, boundary_marker) 
+    else:
+        return None
+
+def search(pattern, string, flags=0, boundaries=False, boundary_marker=';'):
+    """Scan through string looking for a match to the pattern, returning
+    a match object, or None if no match was found.
+    
+    kre modification: source string and pattern are linearized prior to 
+    calling re.search"""
+    match = re.search(_linearize(pattern)[0], _linearize(string, boundaries, boundary_marker)[0], flags)
+
+    if match:
+        return _make_match_object(pattern, string, match, boundaries, boundary_marker) 
+    else:
+        return None
+
+def sub(pattern, repl, string, count=0, flags=0, boundaries=False, boundary_marker=';'):
+    """Return the string obtained by replacing the leftmost
+    non-overlapping occurrences of the pattern in string by the
+    replacement repl.  repl can be either a string or a callable;
+    if a string, backslash escapes in it are processed.  If it is
+    a callable, it's passed the match object and must return
+    a replacement string to be used.
+    
+    kre modification: source string and pattern are linearized prior to 
+    calling re.sub()"""
+    pass
+
+def subn(pattern, repl, string, count=0, flags=0, boundaries=False, boundary_marker=';'):
+    """Return a 2-tuple containing (new_string, number).
+    new_string is the string obtained by replacing the leftmost
+    non-overlapping occurrences of the pattern in the source
+    string by the replacement repl.  number is the number of
+    substitutions that were made. repl can be either a string or a
+    callable; if a string, backslash escapes in it are processed.
+    If it is a callable, it's passed the match object and must
+    return a replacement string to be used.
+
+    kre modification: source string and pattern are linearized prior to 
+    calling re.subn()"""
+    pass
+
+def split(pattern, string, maxsplit=0, flags=0, boundaries=False, boundary_marker=';'):
+    """Split the source string by the occurrences of the pattern,
+    returning a list containing the resulting substrings.  If
+    capturing parentheses are used in pattern, then the text of all
+    groups in the pattern are also returned as part of the resulting
+    list.  If maxsplit is nonzero, at most maxsplit splits occur,
+    and the remainder of the string is returned as the final element
+    of the list.
+    
+    kre modification: source string and pattern are linearized prior to 
+    calling re.split()"""
+    pass
+
+def findall(pattern, string, flags=0, boundaries=False, boundary_marker=';'):
+    """Return a list of all non-overlapping matches in the string.
+
+    If one or more capturing groups are present in the pattern, return
+    a list of groups; this will be a list of tuples if the pattern
+    has more than one group.
+
+    Empty matches are included in the result.
+
+    kre modification: source string and pattern are linearized prior to 
+    calling re.findall()"""
+    match = re.findall(_linearize(pattern)[0], _linearize(string, boundaries, boundary_marker)[0], flags)
+
+    if match:
+        regex = compile(_linearize(pattern)[0])
+        linearized_string, index_mapping = _linearize(string, boundaries, boundary_marker)
+        pos = 0 
+        match_list = []
+        for item in match:
+            sub_match = regex.search(linearized_string, pos)
+            source_string_span = _get_span(sub_match, index_mapping)
+            match_list.append(string[source_string_span[0]:source_string_span[1]])
+            pos = sub_match.span()[1]
+        return match_list
+    else:
+        return None
+
+def finditer(pattern, string, flags=0, boundaries=False, boundary_marker=';'):
+    """Return an iterator over all non-overlapping matches in the
+    string.  For each match, the iterator returns a match object.
+
+    Empty matches are included in the result.
+     
+    kre modifications: source string and pattern are linearized prior to 
+    calling re.finditer()
+    returns list_iterator rather than callable_iterator"""
+
+    #Implementation differs from re.finditer
+    match = re.findall(_linearize(pattern)[0], _linearize(string, boundaries, boundary_marker)[0], flags)
+
+    if match:
+        regex = compile(_linearize(pattern)[0])
+        linearized_string, index_mapping = _linearize(string, boundaries, boundary_marker)
+        pos = 0 
+        match_list = []
+        for item in match:
+            sub_match = regex.search(linearized_string, pos)
+            match_list.append(_make_match_object(pattern, string, sub_match))
+            pos = sub_match.span()[1]
+        return iter(match_list)
+    else:
+        return None
+
+
+def compile(pattern, flags=0):
+    """Compile a regular expression pattern, returning a pattern object.
+
+    kre modification: source string and pattern are linearized prior to 
+    calling re.compile()"""
+    return re.compile(_linearize(pattern)[0], flags)
+    
+
+def purge():
+    """Compile a regular expression pattern, returning a pattern object.
+
+    kre modification: none"""
+    re.purge()
+
+def template(pattern, flags=0):    
+    """Compile a regular expression pattern, returning a pattern object.
+
+    kre modification: source string and pattern are linearized prior to 
+    calling re.template()"""
+    pass
+
+def escape(pattern):
+    """
+    Escape all the characters in pattern except ASCII letters, numbers and '_'.
+
+    kre modification: source string and pattern are linearized prior to 
+    calling re.escape()"""   
+    pass
+
+# --------------------------------------------------------------------
+# internals
+
+#Prepare the text, including re expression, by splitting up Korean syllables into individual Korean letters.  By default, syllable boundaries are unmarked, though I intend to implement an optional argument in the future to allow for syllable boundary marking.  (For the time being, one can already distinguish between onsets and codas using a somewhat cumbersome regex syntax).
+
+def _linearize(string, boundaries=False, boundary_marker=';'):
+    letter_by_letter = ''
+    index_list = []
+    real_index = 0
+    new_index = 0
+    for word in string:
+        for symbol in word:
+            if KO.isSyllable(symbol) == True:
+                for letter in KO.split(symbol):
+                    if letter != '':
+                        letter_by_letter = letter_by_letter + letter
+                        index_list.append([new_index, real_index])
+                        new_index += 1
+                if boundaries==True:
+                    letter_by_letter = letter_by_letter + boundary_marker
+                    index_list.append([new_index, real_index])
+                    new_index += 1
+            else:
+                letter_by_letter = letter_by_letter + symbol
+                index_list.append([new_index, real_index])
+                new_index += 1
+
+        real_index += 1
+ 
+    return (letter_by_letter, index_list)
+
+def _get_span(match, index_mapping):
+    '''Map the index positions of the match to their corresponding positions in the source text''' 
+    span_index = list(match.span())
+    return (index_mapping[span_index[0]][1], index_mapping[span_index[1]-1][1] + 1)
+
+def _make_match_object(pattern, string, match, boundaries=False, boundary_marker=';'):
+    
+    linearized_string, index_mapping = _linearize(string, boundaries, boundary_marker)
+    match_obj = KRE_Match(
+            kre = re.compile(_linearize(pattern)[0]), 
+            re = re.compile(pattern), 
+            string = string, 
+            kre_string = linearized_string, 
+            regs = (_get_span(match, index_mapping),), 
+            index_mapping = index_mapping
+            )
+    return match_obj 
