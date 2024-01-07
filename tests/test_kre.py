8a42ed3f75e6592306f799bdbc950a77912608f9
darrelllarsen <larsendarrell@gmail.com>
2024-01-06 22:17:31 -0500

    Added tests for KRE_Match class attributes and functions.)

--- tests/test_kre.py
+++ tests/test_kre.py
@@ -1,9 +1,11 @@
-import pytest
+import pytest, re
 import korean.kre as kre
 
 arirang = "아리랑 아리랑 아라리요. 아리랑 고개로 넘어간다. 나를 버리고 가시는 님은 십리도 못가서 발병 난다."
 
 news = '월간<산>은 무명명산들을 독자들의 도움을 통해 하나씩 찾아나간다. 국립, 군립, 도립공원 및 100대 명산에 해당하지 않는 산이면서 또 산행하는 것이 불법이 아닌 산이 대상이다. 직접 제보한 독자와 함께 오른다. 제보는 blackhouse@chosun.com 찬란했다. 분명히 빛나고 있었다. 햇빛이 들지 않는 깊은 산골인데도 눈이 부셨다. 구불거리는 계곡을 따라 흘러온 티 없이 투명한 물이 바위에 부딪쳐 맑게 부서지며 빛의 파편을 흩뿌린다. 그 광경에 눈이 멀 것 같아 잠시 이끼 낀 바위에 눈을 얹혀놓고 쉬어본다. 그러자 이번엔 계곡이 산의 정수리부터 꼭 잡아 끌고 내려온 냉기를 온 몸에 퍼붓는다. 살짝 솟았던 땀이 꼼짝없이 얼어붙는다. 귀를 앵앵거리는 날벌레도, 시끄러운 트로트 소리도 없는, 고요한 오지계곡의 호사다. 충북 제천 십자봉(983m)은 완전히 알려지지 않은 산은 아니다. 최근 등산을 시작한 이들에게는 깜깜할지 몰라도 약 30~40년 전부터 꾸준히 산악인들이 찾아온 바 있다. 십자봉을 제보해 준 독자 박노원씨는 "십자봉이 있는 제천시 백운면은 내 고향 시골"이라며 "현재 내 나이가 50대 후반인데 고등학교 무렵부터 산악인들이 찾기 시작했던 것으로 기억한다"고 전했다. 특히 발치에 흐르는 덕동계곡이 2000년대 이후 조용히 다녀오기 좋은 피서지로 유명세를 탔기에 이를 낀 십자봉의 존재도 덩달아 상기되곤 했다. 그래서 여름 무더위에 가기 딱 좋은 산으로 꼽힌다. 이처럼 이름값이 있지만 그래도 여전히 오지의 산임은 분명하다. 제천과 충주, 원주 사이에 있으며 서쪽에 미륵산, 북쪽에 백운산과 치악산에 둘러싸인 형세이기 때문에 개발의 손길이 거의 미치지 않았고, 등산객의 답압이 자연의 성장속도를 이기지 못해 등산로 대부분 수풀이 우거져 있다.'
+
+nonsense = '할ㄱ으하느늘근ㅡ'
 
 ### Public Functions
 
@@ -12,6 +14,8 @@ def test_search():
     assert res.group() == "산들을 독"
     res = kre.search('ㅏㄴ[ㄱ-ㅣ >]+독', news)
     assert res.group() == "산>은 무명명산들을 독"
+    res = kre.search('(ㄹ).*(ㄴ)', arirang)
+    assert res.regs == ((1, 54), (1, 2), (53, 54))
 
 def test_match():
     res = kre.match('ㅏㄴ[ㄱ-ㅣ ]+독', news)
@@ -31,7 +35,6 @@ def test_sub():
 
 
 def test_subn():
-    nonsense = '할ㄱ으하느늘근ㅡ'
     res = kre.subn('느', '나가', nonsense)
     assert res == ('할ㄱ으하나가나갈그나가', 3)
 
@@ -109,9 +112,9 @@ def test_purge():
 def test_escape():
     pass
 
-### KRE_Match Method Calls
+### KRE_Pattern Method Calls
 
-def test_kre_match_search():
+def test_kre_pattern_search():
     m = kre.compile('ㅏㄴ[ㄱ-ㅣ ]+독')
     res = m.search(news)
     assert res.group() == "산들을 독"
@@ -119,7 +122,7 @@ def test_kre_match_search():
     res = m.search(news)
     assert res.group() == "산>은 무명명산들을 독"
 
-def test_kre_match_match():
+def test_kre_pattern_match():
     m = kre.compile('ㅏㄴ[ㄱ-ㅣ ]+독')
     res = m.match(news)
     assert res == None
@@ -130,20 +133,22 @@ def test_kre_match_match():
     assert res.start() == 0
     assert res.end() == 24
 
-def test_kre_match_fullmatch():
+def test_kre_pattern_fullmatch():
     pass
 
-def test_kre_match_sub():
+def test_kre_pattern_sub():
     m = kre.compile('ㄴ다')
     res = m.sub('ㄹ 거예요', arirang)
     assert res == "아리랑 아리랑 아라리요. 아리랑 고개로 넘어갈 거예요. 나를 버리고 가시는 님은 십리도 못가서 발병 날 거예요."
 
-def test_kre_match_subn():
-    nonsense = '할ㄱ으하느늘근ㅡ'
-
+def test_kre_pattern_subn():
     m = kre.compile('느')
     res = m.subn('나가', nonsense)
     assert res == ('할ㄱ으하나가나갈그나가', 3)
+
+    m = kre.compile(';느')
+    res = m.subn(';나가', nonsense, boundaries=True)
+    assert res == ('할ㄱ으하나가나갈근ㅡ', 2)
 
     m = kre.compile('ㅏ')
     res = m.subn('ㅗ',nonsense)
@@ -162,10 +167,10 @@ def test_kre_match_subn():
     res = m.subn('ㅓ', nonsense, syllabify='full')
     assert res == ('핡어하너널거너', 5)
 
-def test_kre_match_split():
+def test_kre_pattern_split():
     pass
 
-def test_kre_match_findall():
+def test_kre_pattern_findall():
     m = kre.compile('ㅏㄹ')
     results = m.findall(arirang)
     assert results == ['아리', '아리', '아라', '라리', '아리', '나를', '발']
@@ -202,7 +207,181 @@ def test_kre_match_findall():
     res = m.findall(arirang, boundaries=True, delimiter='%')
     assert res == ['를', '발']
 
-def test_kre_match_finditer():
+def test_kre_pattern_finditer():
     pass
+
+
+### KRE_Match object attributes and functions
+def test_kre_match_object():
+    # Test case: match of search is syllable, so result should be 
+    # identical to standard re search
+
+    # Pattern includes named and unnamed groups, as well as a named
+    # group with 0 matches
+    p = kre.compile('(?P<첫째>ㄱ)(으)(?P<h>h)?.*(?P<둘째>근)')
+    m = p.search(nonsense)
+    re_p = re.compile('(?P<첫째>ㄱ)(으)(?P<h>h)?.*(?P<둘째>근)')
+    re_m = re_p.search(nonsense)
+
+    # attributes
+    assert m.pos == re_m.pos
+    assert m.endpos == re_m.endpos
+    # assert m.re == re_m.re # WILL CHANGE
+    assert m.regs == re_m.regs
+    assert m.string == re_m.string
+    assert m.lastgroup == re_m.lastgroup
+    assert m.lastindex == re_m.lastindex
+
+    # methods
+    assert m.end() == re_m.end()
+    assert m.start() == re_m.start()
+    assert m.groups() == re_m.groups()
+    assert m.span() == re_m.span()
+    # Change default value for unmatched groups (default is None)
+    assert m.groups('changed') == re_m.groups('changed')
+    for n in range(len(m.groups())+1):
+        assert m.end(n) == re_m.end(n)
+        assert m.start(n) == re_m.start(n)
+        assert m.group(n) == re_m.group(n)
+        assert m.span(n) == re_m.span(n)
+    assert m.groupdict() == re_m.groupdict()
+    # Change default value for unmatched named groups (default is None)
+    assert m.groupdict('changed') == re_m.groupdict('changed')
+
+    # not yet implemented
+    #assert m.expand() == re_m.expand()
+
+def test_kre_match_object2():
+    # Test case: match of search is subsyllable, so result should be 
+    # identical to standard re search
+
+    # Pattern includes named and unnamed groups, as well as a named
+    # group with 0 matches
+    # kre searches for subsyllables should have same results as re
+    # searches for syllables containing said subsyllables
+    p = kre.compile('(?P<첫째>ㄱ).(ㅡ)(?P<h>h)?.*(?P<둘째>그)')
+    m = p.search(nonsense)
+    re_p = re.compile('(?P<첫째>ㄱ)(으)(?P<h>h)?.*(?P<둘째>근)')
+    re_m = re_p.search(nonsense)
+
+    # attributes
+    assert m.pos == re_m.pos
+    assert m.endpos == re_m.endpos
+    # assert m.re == re_m.re # WILL CHANGE
+    assert m.regs == re_m.regs
+    assert m.string == re_m.string
+    assert m.lastgroup == re_m.lastgroup
+    assert m.lastindex == re_m.lastindex
+
+    # methods
+    assert m.end() == re_m.end()
+    assert m.start() == re_m.start()
+    assert m.groups() == re_m.groups()
+    assert m.span() == re_m.span()
+    # Change default value for unmatched groups (default is None)
+    assert m.groups('changed') == re_m.groups('changed')
+    for n in range(len(m.groups())+1):
+        assert m.end(n) == re_m.end(n)
+        assert m.start(n) == re_m.start(n)
+        assert m.group(n) == re_m.group(n)
+        assert m.span(n) == re_m.span(n)
+    assert m.groupdict() == re_m.groupdict()
+    # Change default value for unmatched named groups (default is None)
+    assert m.groupdict('changed') == re_m.groupdict('changed')
+
+    # not yet implemented
+    #assert m.expand() == re_m.expand()
+
+def test_kre_match_object3():
+    # Test case: match of subsyllables pattern that crosses syllables. 
+    # Result should be identical to standard re search off all involved
+    # syllables
+
+    # Pattern includes named and unnamed groups, as well as a named
+    # group with 0 matches
+    # kre searches for subsyllables should have same results as re
+    # searches for syllables containing said subsyllables
+    p = kre.compile('(?P<첫째>ㄱ.ㅡ)(?P<h>h)?.*(?P<둘째>그)')
+    m = p.search(nonsense)
+    re_p = re.compile('(?P<첫째>ㄱ으)(?P<h>h)?.*(?P<둘째>근)')
+    re_m = re_p.search(nonsense)
+
+    # attributes
+    assert m.pos == re_m.pos
+    assert m.endpos == re_m.endpos
+    # assert m.re == re_m.re # WILL CHANGE
+    assert m.regs == re_m.regs
+    assert m.string == re_m.string
+    assert m.lastgroup == re_m.lastgroup
+    assert m.lastindex == re_m.lastindex
+
+    # methods
+    assert m.end() == re_m.end()
+    assert m.start() == re_m.start()
+    assert m.groups() == re_m.groups()
+    assert m.span() == re_m.span()
+    # Change default value for unmatched groups (default is None)
+    assert m.groups('changed') == re_m.groups('changed')
+    for n in range(len(m.groups())+1):
+        assert m.end(n) == re_m.end(n)
+        assert m.start(n) == re_m.start(n)
+        assert m.group(n) == re_m.group(n)
+        assert m.span(n) == re_m.span(n)
+    assert m.groupdict() == re_m.groupdict()
+    # Change default value for unmatched named groups (default is None)
+    assert m.groupdict('changed') == re_m.groupdict('changed')
+
+    # not yet implemented
+    #assert m.expand() == re_m.expand()
+
+def test_kre_match_object4():
+    # Test case: match of subsyllables pattern that crosses syllables. 
+    # Result should be identical to standard re search off all involved
+    # syllables
+
+    # Pattern includes named and unnamed groups, as well as a named
+    # group with 0 matches
+    # kre searches for subsyllables should have same results as re
+    # searches for syllables containing said subsyllables
+    p = kre.compile('(?P<첫째>ㄱ;.ㅡ)(?P<h>h)?.*(?P<둘째>그)')
+    m = p.search(nonsense, boundaries=True)
+    re_p = re.compile('(?P<첫째>ㄱ으)(?P<h>h)?.*(?P<둘째>근)')
+    re_m = re_p.search(nonsense)
+
+    # attributes
+    assert m.pos == re_m.pos
+    assert m.endpos == re_m.endpos
+    # assert m.re == re_m.re # WILL CHANGE
+    assert m.regs == re_m.regs
+    assert m.string == re_m.string
+    assert m.lastgroup == re_m.lastgroup
+    assert m.lastindex == re_m.lastindex
+
+    # methods
+    assert m.end() == re_m.end()
+    assert m.start() == re_m.start()
+    assert m.groups() == re_m.groups()
+    assert m.span() == re_m.span()
+    # Change default value for unmatched groups (default is None)
+    assert m.groups('changed') == re_m.groups('changed')
+    for n in range(len(m.groups())+1):
+        assert m.end(n) == re_m.end(n)
+        assert m.start(n) == re_m.start(n)
+        assert m.group(n) == re_m.group(n)
+        assert m.span(n) == re_m.span(n)
+    assert m.groupdict() == re_m.groupdict()
+    # Change default value for unmatched named groups (default is None)
+    assert m.groupdict('changed') == re_m.groupdict('changed')
+
+    # not yet implemented
+    #assert m.expand() == re_m.expand()
+
+### _Linear
+def test_kre_linear():
+    ls = kre._Linear('This is 한글ㅋㅋ.', boundaries=True)
+    assert ls.delimited == 'This is ;한;글;ㅋ;ㅋ;.'
+    assert ls.del2orig == (0, 1, 2, 3, 4, 5, 6, 7, None, 8, None, 9, 
+            None, 10, None, 11, None, 12)
+
 
 ### KRE_Pattern Method Calls
